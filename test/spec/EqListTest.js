4a389d73d9c31b2ba5e44d4be9f10f0d82521988
Hal Simpson <hasimpson@usgs.gov>
2013-10-23 13:04:46 -0600

    First go round for a web-lib for EqList, formally sig/sigeq.js.

---
+++ test/spec/EqListTest.js
@@ -0,0 +1,25 @@
+/* global define, describe, it */
+define([
+	'chai',
+	'EqList'
+], function (
+	chai,
+	EqList
+) {
+	'use strict';
+	var expect = chai.expect;
+
+	describe('EqList', function () {
+		describe('Constructor', function () {
+			it('Is defined', function () {
+				/* jshint -W030 */
+				expect(EqList).not.to.be.null;
+				/* jshint +W030 */
+			});
+			it('Is instantiated', function () {
+				var eqList = new EqList();
+				expect(eqList).to.be.an.instanceof(EqList);
+			});
+		});
+	});
+});
