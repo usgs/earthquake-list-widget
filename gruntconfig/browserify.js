6dd6414c6cbc093f7e3d0eb3fd4e50032fea0ec4
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-04 12:28:04 -0700

    added exports to drowserify

--- gruntconfig/browserify.js
+++ gruntconfig/browserify.js
@@ -2,8 +2,38 @@
 
 var config = require('./config');
 
-var browserify = {
+var EXPORTS = [
+  'listwidget/DYFIList',
+  'listwidget/EqList',
+  'listwidget/PAGERList',
+  'listwidget/ShakeMapList'
+];
 
+var browserify = {
+  options: {
+    browserifyOptions: {
+      debug: true,
+      paths: [
+        process.cwd() + '/' + config.src
+      ]
+    }
+  },
+  source: {
+    src: []
+    dest: config.build + '/' + config.src + '/DYFIList.js',
+    options: {
+      alias: EXPORTS.map (function ( path) {
+        return './' + config.src + '/' + path + '.js:' + path
+      })
+    }
+  },
+  test: {
+    src: config.test + '/test.js',
+    dest: config.build + '/' + config.test + '/test.js',
+    options: {
+      exclude: EXPORTS
+    }
+  }
 };
 
 module.exports = browserify;
