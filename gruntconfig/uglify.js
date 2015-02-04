108bfbc806be1db8dabf1f86fc264818dd41f921
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-04 15:19:51 -0700

    changed setup

--- gruntconfig/uglify.js
+++ gruntconfig/uglify.js
@@ -3,15 +3,12 @@
 var config = require('./config');
 
 var uglify = {
-  build: {
-    files: {
-    }
+  options: {
+  },
+  dist: {
+    src: config.build + '/' + config.src + '/hazdev-list-widget.js',
+    dest: config.dist + '/hazdev-listw-idget.js'
   }
 };
-
-// uglify.build.files[config.dist + '/index.js'] =
-//     config.build + '/example/index.js';
-uglify.build.files[config.dist + '/*.js'] =
-    config.build + '/src/*.js';
 
 module.exports = uglify;
