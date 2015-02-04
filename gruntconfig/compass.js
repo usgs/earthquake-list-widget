b4134acc4196fb08afa7b639f0a41317a90ea841
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-04 11:22:45 -0700

    created and set up compass file for grunt

---
+++ gruntconfig/compass.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var config = require('./config');
+
+var compass = {
+  dev: {
+    options: {
+      sassDir: config.src,
+      cssDir: config.build + '/src',
+      environment: 'development'
+    }
+  },
+  test: {
+    options: {
+      sassDir: config.test,
+      cssDir: config.build + '/test',
+      environment: 'development'
+    }
+  }
+};
+
+module.exports = compass;
