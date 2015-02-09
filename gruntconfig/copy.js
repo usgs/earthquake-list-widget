839a6d3f30474c6118891513654b8b36acfd813d
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-09 08:16:21 -0700

    removed example

--- gruntconfig/copy.js
+++ gruntconfig/copy.js
@@ -8,18 +8,6 @@ var copy = {
     cwd: config.test,
     src: '*.html',
     dest: config.build + '/test'
-  },
-  // example: {
-  //   expand: true,
-  //   cwd: config.example,
-  //   src: '*.html',
-  //   dest: config.build + '/example'
-  // },
-  mocha: {
-    expand: true,
-    cwd: 'node_modules',
-    src: ['mocha/mocha.js', 'mocha/mocha.css'],
-    dest: config.build + '/' + config.test
   }
 };
 
