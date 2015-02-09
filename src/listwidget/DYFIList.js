32807a40069e49f1dda43ab68dfe1c25f28d0657
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-09 08:19:33 -0700

    updated to codeing standards

--- src/listwidget/DYFIList.js
+++ src/listwidget/DYFIList.js
@@ -1,37 +1,51 @@
 'use strict';
-// not sure if this will work
-var EqList;
 
-EqList = './EqList';
+var EqList = require('./EqList');
 
-//end of ?
+var DYFIList = function (container, feed, options) {
+  var _this,
+      _initialize,
 
-var DYFIList = function () {
-  EqList.apply(this, arguments);
-};
-DYFIList.prototype = Object.create(EqList.prototype);
+      _autoload;
 
-DYFIList.prototype._getClassName = function () {
-  return 'DYFIList';
-};
+  options = options || {};
+  _autoload = (options.load !== false);
+  options.load = false;
 
-DYFIList.prototype._includeEvent = function (e) {
-  return (e.properties.types.indexOf('dyfi') !== -1);
-};
+  _this = EqList(container, feed, options);
+
+  _initialize = function () {
+    if (_autoload) {
+      _this.load();
+
+    }
+  };
+
+  _this._getClassName = function () {
+    return 'DYFIList';
+  };
+
+  _this._includeEvent = function (e) {
+    return (e.properties.types.indexOf('dyfi') !== -1);
+  };
 
 
-DYFIList.prototype._getEventValue = function (e) {
-  var romanCdi = this._decToRoman(e.properties.cdi);
+  _this._getEventValue = function (e) {
+    var romanCdi = _this._decToRoman(e.properties.cdi);
 
-  return '<span class="roman mmi' + romanCdi + '">' + romanCdi + '</span>';
-};
+    return '<span class="roman mmi' + romanCdi + '">' + romanCdi + '</span>';
+  };
 
-DYFIList.prototype._getEventTitle = function (e) {
-  return e.properties.title;
-};
+  _this._getEventTitle = function (e) {
+    return e.properties.title;
+  };
 
-DYFIList.prototype._getEventAside = function (e) {
-  return (e.properties.felt||0) + ' responses';
+  _this._getEventAside = function (e) {
+    return (e.properties.felt||0) + ' responses';
+  };
+
+  _initialize();
+  return _this;
 };
 
 module.exports = DYFIList;
