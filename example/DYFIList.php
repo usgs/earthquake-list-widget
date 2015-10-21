b7f7a02e6d76b557e0e5f60f8ef229ae02cf218f
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-10-21 15:11:44 -0600

    moved script to its own js file

--- example/DYFIList.php
+++ example/DYFIList.php
@@ -12,9 +12,15 @@ include '_example.inc.php';
 
 <h2>Other Formats</h2>
 <ul>
-  <li><a href="EqList.php<?php echo $templateLink ?>">Default Eathquake List</a></li>
-  <li><a href="ShakeMapList.php<?php echo $templateLink ?>">ShakeMap List</a></li>
-  <li><a href="PAGERList.php<?php echo $templateLink ?>">PAGER List</a></li>
+  <li>
+    <a href="EqList.php<?php echo $templateLink ?>">Default Eathquake List</a>
+  </li>
+  <li>
+    <a href="ShakeMapList.php<?php echo $templateLink ?>">ShakeMap List</a>
+  </li>
+  <li>
+    <a href="PAGERList.php<?php echo $templateLink ?>">PAGER List</a>
+  </li>
 </ul>
 
 <section class="pastday list">
