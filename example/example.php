e38cf98bfe1e00c442ddf9dc2de0fa30ad8361fe
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-10-21 15:16:45 -0600

    fixed code format

--- example/example.php
+++ example/example.php
@@ -7,8 +7,16 @@ include '_example.inc.php';
 ?>
 <h1>Earthquake List Widget Examples</h1>
 <ul>
-  <li><a href="EqList.php<?php echo $templateLink ?>">Default Eathquake List</a></li>
-  <li><a href="DYFIList.php<?php echo $templateLink ?>">DYFI List</a></li>
-  <li><a href="ShakeMapList.php<?php echo $templateLink ?>">ShakeMap List</a></li>
-  <li><a href="PAGERList.php<?php echo $templateLink ?>">PAGER List</a></li>
+  <li>
+    <a href="EqList.php<?php echo $templateLink ?>">Default Eathquake List</a>
+  </li>
+  <li>
+    <a href="DYFIList.php<?php echo $templateLink ?>">DYFI List</a>
+  </li>
+  <li>
+    <a href="ShakeMapList.php<?php echo $templateLink ?>">ShakeMap List</a>
+  </li>
+  <li>
+    <a href="PAGERList.php<?php echo $templateLink ?>">PAGER List</a>
+  </li>
 </ul>
