0e73ecb1ebb2f0f22597ef459dbc659f12381e17
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-10-21 15:13:50 -0600

    moved file to its own js file

--- example/EqList.php
+++ example/EqList.php
@@ -1,40 +1,30 @@
 <?php
 
-if(!isset($TEMPLATE)) {
+if (!isset($TEMPLATE)) {
   $TITLE = 'Default Earthquake List';
-  $HEAD = '<link rel="stylesheet" href="EqList.css"/>';
-  $FOOT = '<script src="EqList.js"></script>';
+  $HEAD = '<link rel="stylesheet" href="earthquake-list-widget.css"/>';
+  $FOOT = '<script src="earthquake-list-widget.js"></script>' .
+      '<script src="EqList.js"></script>';
 }
 
 include '_example.inc.php';
 
 ?>
 
-<div id="example">
+<h2>Other Formats</h2>
+<ul>
+  <li>
+    <a href="DYFIList.php<?php echo $templateLink ?>">DYFI List</a>
+  </li>
+  <li>
+    <a href="ShakeMapList.php<?php echo $templateLink ?>">ShakeMap List</a>
+  </li>
+  <li>
+    <a href="PAGERList.php<?php echo $templateLink ?>">PAGER List</a>
+  </li>
+</ul>
 
-  <h2>Other Formats</h2>
-  <ul>
-    <li><a href="DYFIList.php<?php echo $templateLink ?>">DYFI List</a></li>
-    <li><a href="ShakeMapList.php<?php echo $templateLink ?>">ShakeMap List</a></li>
-    <li><a href="PAGERList.php<?php echo $templateLink ?>">PAGER List</a></li>
-  </ul>
-
-  <section class="list">
-    <h2>M2.5+ Earthquakes Worldwide, Past Day</h2>
-    <div id="list"></div>
-  </section>
-
-  <script src="earthquake-list-widget.js"></script>
-  <script>
-  'use strict';
-
-  var EqList = require('listwidget/EqList');
-
-  EqList({
-    container: document.querySelector('#list'),
-    feed: EqList.M25_URL_DAY
-  });
-
-  </script>
-
-</div>
+<section class="list">
+  <h2>M2.5+ Earthquakes Worldwide, Past Day</h2>
+  <div id="list"></div>
+</section>
