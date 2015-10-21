46122993d9ce72a3d475a1834b79b3f606312ff3
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-10-21 15:14:42 -0600

    moved script to its own js file

--- example/PAGERList.php
+++ example/PAGERList.php
@@ -2,37 +2,28 @@
 
 if (!isset($TEMPLATE)) {
   $TITLE = 'Pager List';
-  $HEAD = '<link rel="stylesheet" href="PAGERList.css"/>';
-  $FOOT = '<script src="PAGERList.js"></script>';
+  $HEAD = '<link rel="stylesheet" href="earthquake-list-widget.css"/>';
+  $FOOT = '<script src="earthquake-list-widget.js"></script>' .
+      '<script src="PAGERList.js"></script>';
 }
 include '_example.inc.php';
 
 ?>
 
-<div id="example">
-  <h2>Other Formats</h2>
-  <ul>
-    <li><a href="EqList.php<?php echo $templateLink ?>">Default Eathquake List</a></li>
-    <li><a href="DYFIList.php<?php echo $templateLink ?>">DYFI List</a></li>
-    <li><a href="ShakeMapList.php<?php echo $templateLink ?>">ShakeMap List</a></li>
-  </ul>
+<h2>Other Formats</h2>
+<ul>
+  <li>
+    <a href="EqList.php<?php echo $templateLink ?>">Default Eathquake List</a>
+  </li>
+  <li>
+    <a href="DYFIList.php<?php echo $templateLink ?>">DYFI List</a>
+  </li>
+  <li>
+    <a href="ShakeMapList.php<?php echo $templateLink ?>">ShakeMap List</a>
+  </li>
+</ul>
 
-  <section class="list">
-    <h2>Matching Results Past 7 Days</h2>
-    <div id="list"></div>
-  </section>
-
-  <script src="earthquake-list-widget.js"></script>
-  <script>
-  'use strict';
-
-  var EqList = require('listwidget/EqList'),
-      PAGERList = require('listwidget/PAGERList');
-
-  PAGERList({
-    container: document.querySelector('#list'),
-    feed: EqList.ALL_URL_WEEK
-  });
-
-  </script>
-</div>
+<section class="list">
+  <h2>Matching Results Past 7 Days</h2>
+  <div id="list"></div>
+</section>
